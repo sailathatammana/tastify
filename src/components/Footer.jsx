b8f2e39f054091a7fabd4b72761b334a359b2214
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2021-09-10 21:35:38 +0200

    updated footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,18 @@
-import Github from "../assets/icons/github.jpg";
-import Linkedin from "../assets/icons/linkedin.jpg";
+import Facebook from "../assets/icons/facebook.png";
+import Instagram from "../assets/icons/instagram.png";
 
 export default function Footer() {
   return (
     <footer className="footer">
       <ul>
         <li>
-          <a
-            href="https://www.linkedin.com/in/sai-latha-tammana/"
-            target="blank"
-          >
-            <img src={Linkedin} alt="linkedin" />
+          <a href="https://www.facebook.com/" target="blank">
+            <img src={Facebook} alt="facebook" />
           </a>
         </li>
         <li>
-          <a href="https://github.com/sailathatammana" target="blank">
-            <img src={Github} alt="github" />
+          <a href="https://www.instagram.com/" target="blank">
+            <img src={Instagram} alt="instagram" />
           </a>
         </li>
       </ul>
